9ad962d214324d2d45d0afbf5bc0cf21a5118561
zfletch <zfletch2@gmail.com>
2017-04-30 17:29:19 -0400

    use npm for beta-code-js

--- bin/chrome_build.sh
+++ bin/chrome_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat ./beta-code-js/bundle/beta-code.js \
+cat ./node_modules/beta-code-js/bundle/beta-code.js \
   > ./chrome_extension/compiled.js
 
 cp ./icons/*.png ./chrome_extension/
--- bin/firefox_build.sh
+++ bin/firefox_build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-cat ./beta-code-js/bundle/beta-code.js \
+cat ./node_modules/beta-code-js/bundle/beta-code.js \
   > ./firefox_extension/compiled.js
 
 cp ./icons/*.png ./firefox_extension/
-
